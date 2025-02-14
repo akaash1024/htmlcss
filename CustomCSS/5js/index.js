2d7dada1d32e63d8b2b67830356a5bac12bd6b73
akashkewat99 <akashkewat99@gmail.com>
2025-02-14 19:05:28 +0530

    updated

--- CustomCSS/5js/index.js
+++
@@ -1,9 +0,0 @@
-var root = document.querySelector(':root');
-var rootStyles = getComputedStyle(root);
-var red = rootStyles.getPropertyValue('--red');
-console.log('red: ', red);
-
-
-root.style.setProperty('--red', 'green');
-
-// root.style.setProperty('--yellow', 'teal') /* for practice ignore */
